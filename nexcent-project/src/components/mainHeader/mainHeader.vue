38584cdd050c405bf76934f89e9ffd93ceee0c13
Khetag <pubgccntgml@gmail.com>
2025-09-20 19:24:52 +0300

    Header finished

--- nexcent-project/src/components/mainHeader/mainHeader.vue
+++ nexcent-project/src/components/mainHeader/mainHeader.vue
@@ -5,18 +5,28 @@
                 <img src="@/assets/images/Logo.png" alt="Nexcent Logo">
             </router-link>
             
-            <nav class="header__navigation">
+            <nav class="header__navigation" :class="{ 'active': isMenuOpen }" @click="closeMenu">
                 <ul class="header__list">
                     <li v-for="item in items" :key="item.id">
                         <router-link 
                         :to="item.path" 
                         class="header__list-el"
                         :class="{ 'active-link': $route.path === item.path }"
+                        @click="closeMenu"
                         >
                         <span>{{ item.name }}</span>
                     </router-link>
                 </li>
             </ul>
+            
+            <div class="mobile-menu-buttons">
+                <router-link to="/#login" class="header__entrance-el-login" @click="closeMenu">
+                    <span class="header__entrance-text-login">Login</span>
+                </router-link>
+                <router-link to="/#signUp" class="header__entrance-el-signUp" @click="closeMenu">
+                    <span class="header__entrance-text-signUp">Sign up</span>
+                </router-link>
+            </div>
         </nav>
         
         <div class="header__entrance">
@@ -33,15 +43,6 @@
             <div class="burger__div-center"></div>
             <div></div>
         </div>
-        
-        <!-- <div class="mobile-menu-buttons">
-            <router-link to="/#login" class="header__entrance-el-login" @click="closeMenu">
-                <span class="header__entrance-text-login">Login</span>
-            </router-link>
-            <router-link to="/#signUp" class="header__entrance-el-signUp" @click="closeMenu">
-                <span class="header__entrance-text-signUp">Sign up</span>
-            </router-link>
-        </div> -->
         
     </div>
 </header>
@@ -71,7 +72,7 @@ const closeMenu = () =>
 
 watch(route, () => 
 {
-    closeMenu
+    closeMenu()
 })
 
 </script>
