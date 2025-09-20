06c590bc72a1c463848ce86c32e55e57166490b6
Khetag <pubgccntgml@gmail.com>
2025-09-20 16:14:43 +0300

    header mobile burger

--- nexcent-project/src/components/mainHeader/mainHeader.vue
+++ nexcent-project/src/components/mainHeader/mainHeader.vue
@@ -14,32 +14,66 @@
                         :class="{ 'active-link': $route.path === item.path }"
                         >
                         <span>{{ item.name }}</span>
-                        </router-link>
-                    </li>
-                </ul>
-            </nav>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
         
-            <div class="header__entrance">
-                <router-link to="/#login" class="header__entrance-el-login">
-                    <span class="header__entrance-text-login">Login</span>
-                </router-link>
-                <router-link to="/#signUp" class="header__entrance-el-signUp">
-                    <span class="header__entrance-text-signUp">Sign up</span>
-                </router-link>
-            </div>
+        <div class="header__entrance">
+            <router-link to="/#login" class="header__entrance-el-login">
+                <span class="header__entrance-text-login">Login</span>
+            </router-link>
+            <router-link to="/#signUp" class="header__entrance-el-signUp">
+                <span class="header__entrance-text-signUp">Sign up</span>
+            </router-link>
         </div>
+        
+        <div class="header__burger-mobile" :class="{ 'active': isMenuOpen }" @click="toggleMenu">
+            <div></div>
+            <div class="burger__div-center"></div>
+            <div></div>
+        </div>
+        
+        <!-- <div class="mobile-menu-buttons">
+            <router-link to="/#login" class="header__entrance-el-login" @click="closeMenu">
+                <span class="header__entrance-text-login">Login</span>
+            </router-link>
+            <router-link to="/#signUp" class="header__entrance-el-signUp" @click="closeMenu">
+                <span class="header__entrance-text-signUp">Sign up</span>
+            </router-link>
+        </div> -->
+        
+    </div>
 </header>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { computed } from "vue";
+import { watch } from "vue";
 import { useRoute } from 'vue-router'; 
 import { useHeaderStore } from '@/stores/HeaderStore'
 
 const store = useHeaderStore()
 const items = computed(()=>store.items)
 const route = useRoute() 
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => 
+{
+    isMenuOpen.value = !isMenuOpen.value
+}
+
+const closeMenu = () => 
+{
+    isMenuOpen.value = false
+}
+
+watch(route, () => 
+{
+    closeMenu
+})
+
 </script>
 
 <style scoped lang="scss">
