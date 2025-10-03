49b8e62158eae4b0a4e51415f5421ff1015e5184
Khetag <pubgccntgml@gmail.com>
2025-10-03 15:55:16 +0300

    Footer add new files and starting

---
+++ nexcent-project/src/components/mainFooter/mainFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+        <div class="container">
+            
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@use '@/components/mainHeader/mainHeader.scss';
+
+</style>
