4b3e7be1c9e4866294b16328475099351751ad5a
Khetag <pubgccntgml@gmail.com>
2025-10-03 15:47:52 +0300

    section fringilla end

--- nexcent-project/src/components/sections/fringillaFree/fringillaFree.vue
+++ nexcent-project/src/components/sections/fringillaFree/fringillaFree.vue
@@ -1,12 +1,23 @@
 <template>
-    
+    <section class="fringillaFree">
+        <div class="container">
+            <div class="block__flex">
+                <h2 class="block__flex-title">
+                    Pellentesque suscipit fringilla libero eu.
+                </h2>
+
+                <btnComponent :button-id="3" />
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
+import btnComponent from "@/components/common/btnComponent/btnComponent.vue";
 
 </script>
 
 <style scoped lang="scss">
-@use '@/components/sections/ourClients/ourClients.scss'; 
+@use '@/components/sections/fringillaFree/fringillaFree.scss'; 
 
 </style>
